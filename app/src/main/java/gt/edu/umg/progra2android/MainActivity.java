12ca5c85d8239a3cf444ec6d3ef1ee5875c9fbd0
Oliver <ogodoys@miumg.edu.gt>
2024-10-14 09:17:07 -0600

    Aplicación android hecha en clase

--- app/src/main/java/gt/edu/umg/progra2android/MainActivity.java
+++ app/src/main/java/gt/edu/umg/progra2android/MainActivity.java
@@ -1,6 +1,7 @@
 package gt.edu.umg.progra2android;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.Button;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         btnSaludar = findViewById(R.id.btnSaludar);
         tvSaludo = findViewById(R.id.textView);
         btnCrearDb = findViewById(R.id.btnCrearDb);
+        Button btnCrearRegistro = findViewById(R.id.btnCrearRegistro);
 
         btnSaludar.setOnClickListener(v -> {
             Toast.makeText(this, "Hola Oliver", Toast.LENGTH_SHORT).show(); //Envia notificación
@@ -45,10 +47,18 @@ public class MainActivity extends AppCompatActivity {
             SQLiteDatabase db = dbHelper.getWritableDatabase();
             if(db != null){
                 Toast.makeText(this, "Base de Datos Creada", Toast.LENGTH_SHORT).show(); //Envia notificación
-                new DbContactos().insertaContacto("Juan","12345678","ddd.com");
+                new DbContactos(this).insertaContacto("Juan","12345678","ddd.com");
             }else{
                 Toast.makeText(this, "Error al crear la base de datos", Toast.LENGTH_SHORT).show();
             }
+
+        });
+
+        //crear registro llama a la activity NuevoActivity
+        btnCrearRegistro.setOnClickListener(v -> {
+            Toast.makeText(this, "Creando Registro", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, NuevoActivity.class);
+            startActivity(intent);
 
         });
 
--- app/src/main/java/gt/edu/umg/progra2android/NuevoActivity.java
+++ app/src/main/java/gt/edu/umg/progra2android/NuevoActivity.java
@@ -1,6 +1,9 @@
 package gt.edu.umg.progra2android;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +11,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import gt.edu.umg.progra2android.BaseDatos.DbContactos;
+
 public class NuevoActivity extends AppCompatActivity {
 
     //NuevoActivity.Java agregar elultimo archivo
+    Button btnNuevo;
+    EditText txtNombre, txtTelefono, txtEmail;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_nuevo);
+
+        btnNuevo = findViewById(R.id.btnGuardar);
+        txtNombre = findViewById(R.id.txtNombre);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtEmail = findViewById(R.id.txtEmail);
+
+        btnNuevo.setOnClickListener(v -> {
+            DbContactos dbContactos = new DbContactos(this);
+            long id = dbContactos.insertaContacto(txtNombre.getText().toString(), txtTelefono.getText().toString(), txtEmail.getText().toString());
+            if (id > 0) {
+                txtNombre.setText("");
+                txtTelefono.setText("");
+                txtEmail.setText("");
+                Toast.makeText(this, "Contacto guardado", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Error al guardar el contacto", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
